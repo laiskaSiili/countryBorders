f9266c7df4c5e3a68ee15bb372b81072057fbd0d
Marc Folini <m.folini@carbon-delta.com>
2019-06-20 15:17:20 +0200

    Fix location intersection bug

--- django/countryborders/views.py
+++ django/countryborders/views.py
@@ -15,6 +15,7 @@ import urllib, base64
 
 class DrawCountry(View):
 
+    NUM_CITIES = 5
     DF_COUNTRIES = gpd.read_file('countryborders/data/ne_50m_admin_0_countries/ne_50m_admin_0_countries.shp')
     DF_POP_PLACES = gpd.read_file('countryborders/data/ne_10m_populated_places/ne_10m_populated_places.shp')
 
@@ -29,32 +30,33 @@ class DrawCountry(View):
         if form.is_valid():
             country_name = form.cleaned_data['country']
             coordinate_system = form.cleaned_data['coordinate_system']
-            # select country from dataframe
+            # select country from dataframe and also get 5 most populated places for country
             if country_name == 'World':
-                country = DrawCountry.DF_COUNTRIES
+                country = DrawCountry.DF_COUNTRIES.copy()
+                country_cities_top5 = DrawCountry.DF_POP_PLACES.copy().sort_values(by=['POP_MAX'], ascending=False).head(DrawCountry.NUM_CITIES)
             else:
                 country = DrawCountry.DF_COUNTRIES[DrawCountry.DF_COUNTRIES['ADMIN'] == country_name].copy()
-            # get 5 most populated places for country
-            country_cities_top5 = DrawCountry.DF_POP_PLACES[(DrawCountry.DF_POP_PLACES['ADM0NAME']==country_name)].sort_values(by=['POP_MAX'], ascending=False).head(5)
+                country_cities_top5 = DrawCountry.DF_POP_PLACES[(DrawCountry.DF_POP_PLACES['ADM0NAME']==country_name)].copy().sort_values(by=['POP_MAX'], ascending=False).head(DrawCountry.NUM_CITIES)
+            
+            # Reproject data according to projection form field
+            country = country.to_crs(epsg=coordinate_system)
+            country_cities_top5 = country_cities_top5.to_crs(epsg=coordinate_system)
+            
             # if show_largest_only checked, replace geometry with largest polygon
             if form.cleaned_data['show_largest_only']:
                 try:
                     max_area = 0
                     for multipoly in country.geometry:
+                        print('----')
+                        print(len(multipoly))
                         for poly in multipoly:
                             if poly.area > max_area:
                                 largest_poly = poly
                                 max_area = poly.area
                     country['geometry'] = largest_poly
-                    # select only the populated places that intersect with the country polygon
-                    country_cities_top5 = country_cities_top5.loc[country_cities_top5.intersects(country)]
+                    country_cities_top5 = country_cities_top5[country_cities_top5.intersects(largest_poly)]
                 except TypeError:
                     pass
-            
-    
-            # Reproject data according to projection form field
-            country = country.to_crs(epsg=coordinate_system)
-            country_cities_top5 = country_cities_top5.to_crs(epsg=coordinate_system)
 
             # overlay with different markersizes corresponding to the population sizes.
             def get_markersizes(dataseries, min_markersize, max_markersize):
